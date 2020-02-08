83680fa27fc890cf97f46669e9484e738ee7ee74
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2020-02-08 11:24:31 +0530

    Started code

---
+++ day_27_28/starter_code/frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+
+class App extends React.Component {
+  state = {
+    bookDetails: "",
+    error: ""
+  };
+
+  fetchBookDetails = () => {
+    axios
+      .get("/book/23432443252ddddd3")
+      .then(res => {
+        console.log(res.data);
+        this.setState({ bookDetails: JSON.stringify(res.data) });
+      })
+      .catch(err => {
+        this.setState({ error: err.toString() });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Hi from React</p>
+        <p>{this.state.bookDetails}</p>
+        <p>{this.state.error}</p>
+        <button onClick={this.fetchBookDetails}>Fetch Book Data</button>
+      </div>
+    );
+  }
+}
+
+export default App;
